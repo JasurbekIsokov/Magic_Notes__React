240d3c0e91c036f31af22f98753dda722c2579f4
Jasurbek <jasurisoqov685@gmail.com>
2022-04-24 06:36:31 +0500

    Create returnComponent file import and export

--- react-portfolio/src/index.js
+++ react-portfolio/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import NavbarComponent from "./homePage/NavbarComponent.js";
+
+import ReturnComponent from "./homePage/ReturnComponent.js";
 
 class App extends React.Component {
   constructor(props) {
@@ -9,7 +10,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <NavbarComponent />
+        <ReturnComponent />
       </div>
     );
   }
