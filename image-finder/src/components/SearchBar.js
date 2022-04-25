791433bbb435f1ae4eced2807d9b49ecdc7f92e1
Jasurbek <jasurisoqov685@gmail.com>
2022-04-25 16:59:37 +0500

    SearchBar component finihsed

--- image-finder/src/components/SearchBar.js
+++ image-finder/src/components/SearchBar.js
@@ -1,12 +1,36 @@
 import React from "react";
 
 class SearchBar extends React.Component {
+  state = { inputValue: "" };
   constructor(props) {
     super(props);
   }
 
+  getInputData(e) {
+    this.setState({ inputValu: e.target.value });
+  }
+
+  formSubmit = (e) => {
+    e.prevenDefault();
+  };
+
   render() {
-    return <div></div>;
+    return (
+      <div className="ui container" style={{ marginTop: "30px " }}>
+        <form onSubmit={this.formSubmit} className="ui search">
+          <div className="ui icon input" style={{ width: "100%" }}>
+            <input
+              onChange={(e) => this.getInputData(e)}
+              className="prompt"
+              type="text"
+              placeholder="Search images..."
+            />
+            <i className="search icon"></i>
+          </div>
+          <div className="results"></div>
+        </form>
+      </div>
+    );
   }
 }
 
--- image-finder/src/index.js
+++ image-finder/src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 // my components
-
 import App from "./components/App.js";
 
 ReactDOM.render(<App />, document.querySelector("#root"));
